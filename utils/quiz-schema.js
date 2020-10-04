647d156bf7a02d9dfbf68c43cce8d5588c5de56b
emredalkiran <emre59dalkiran@gmail.com>
2020-10-05 00:17:03 +0300

    Simple db querries for quizzes added

--- utils/quiz-schema.js
+++ utils/quiz-schema.js
@@ -4,10 +4,11 @@ const types = require('./types')
 const quizSchema = Joi.object({
   quizName: Joi.string().alphanum().min(3).max(50).required(),
   questions: Joi.array().items({
-    number: Joi.number(),
-    content: Joi.string(),
-    type: Joi.string().validate(...Object.keys(types)),
+    number: Joi.number().required(),
+    content: Joi.string().required(),
+    type: Joi.string().validate(...Object.keys(types)).required(),
   }),
+  creater: Joi.string().required()
 })
 
 module.exports = { quizSchema }
